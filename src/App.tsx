93b8340691573b3c7a1a658a974c39254c88a05e
Leon Christian Habineza <leonchristianhabineza@Leons-MacBook-Pro.local>
2023-02-02 10:59:46 -0600

    set up a typescript app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Home as HomeIcon } from "@mui/icons-material";
+import Home from "./pages/home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeIcon />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import './home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="home">HOME PAGE</h1>
+    </div>
+  )
+}
